bbc3785d2a5cae72d5b59e2b0f9575422e26e153
JakoError <jakozhou@foxmail.com>
2021-09-23 02:05:21 +0800

    Add basic content to Me fragment

--- app/src/main/java/edu/uestc/diaryinuestc/MainActivity.java
+++ app/src/main/java/edu/uestc/diaryinuestc/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_diary, R.id.nav_bill, R.id.nav_todo,R.id.nav_settings)
+                R.id.nav_diary, R.id.nav_bill, R.id.nav_todo,R.id.nav_me)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
---
+++ app/src/main/java/edu/uestc/diaryinuestc/ui/me/MeFragment.java
@@ -0,0 +1,30 @@
+package edu.uestc.diaryinuestc.ui.me;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import edu.uestc.diaryinuestc.databinding.FragmentMeBinding;
+
+public class MeFragment extends Fragment {
+
+    private FragmentMeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentMeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
