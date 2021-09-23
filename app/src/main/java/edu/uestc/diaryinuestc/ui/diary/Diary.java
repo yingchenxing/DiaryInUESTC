33cc2798d69242f3efd1bb621041b289933d227c
yingchenxing <963929975@qq.com>
2021-09-23 10:53:37 +0800

    add diary_item.xml and Diary

--- app/src/main/java/edu/uestc/diaryinuestc/ui/diary/Diary.java
+++ app/src/main/java/edu/uestc/diaryinuestc/ui/diary/Diary.java
@@ -5,4 +5,29 @@ public class Diary {
     private int coverId;
     private String content;
 
+    public Diary (String tittle,int coverId){
+        this.tittle = tittle;
+        this.coverId = coverId;
+    }
+
+    public String getTittle(){
+        return this.tittle;
+    }
+
+    public int getCoverId(){
+        return this.coverId;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public void setTittle (String tittle){
+        this.tittle = tittle;
+    }
+
+    public void setCoverId(int coverId){
+        this.coverId = coverId;
+    }
+
 }
