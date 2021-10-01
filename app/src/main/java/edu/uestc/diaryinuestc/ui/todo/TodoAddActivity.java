875ace41024d87c8fe194292fc09d856d677a199
yingchenxing <963929975@qq.com>
2021-10-01 17:42:05 +0800

    fix todo_add by using popupWindow

--- app/src/main/java/edu/uestc/diaryinuestc/ui/todo/TodoAddActivity.java
+++ app/src/main/java/edu/uestc/diaryinuestc/ui/todo/TodoAddActivity.java
@@ -1,34 +1,26 @@
 package edu.uestc.diaryinuestc.ui.todo;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 import org.greenrobot.eventbus.EventBus;
 
 import edu.uestc.diaryinuestc.R;
 
-public class TodoAddFragment extends Fragment {
-
+public class TodoAddActivity extends AppCompatActivity {
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_add_todo,container,false);
-
-        //保存todo内容
-        EditText editText = view.findViewById(R.id.todo_edit);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_add_todo);
+        EditText editText = findViewById(R.id.todo_edit);
         editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -47,15 +39,14 @@ public class TodoAddFragment extends Fragment {
         });
 
         //保存按钮
-        Button button = view.findViewById(R.id.add_todo_button);
+        Button button = findViewById(R.id.add_todo_button);
 
         button.setOnClickListener(view1 -> {
-            Toast.makeText(getContext(),"已保存",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"已保存",Toast.LENGTH_SHORT).show();
             EventBus.getDefault().post(editText.getText().toString());
+            onBackPressed();
 
-            getActivity().getSupportFragmentManager().popBackStack();
         });
-
-        return view;
     }
+
 }
--- app/src/main/java/edu/uestc/diaryinuestc/ui/todo/TodoFragment.java
+++ app/src/main/java/edu/uestc/diaryinuestc/ui/todo/TodoFragment.java
@@ -1,24 +1,44 @@
 package edu.uestc.diaryinuestc.ui.todo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.widget.Toast;
+import android.widget.PopupWindow;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
 
+import edu.uestc.diaryinuestc.MainActivity;
 import edu.uestc.diaryinuestc.R;
 import edu.uestc.diaryinuestc.databinding.FragmentTodoBinding;
 
@@ -28,6 +48,8 @@ public class TodoFragment extends Fragment {
     private FragmentTodoBinding binding;
     private List<Todo> todoList = new ArrayList<>();
     private TodoAdapter adapter;
+    private SwipeRefreshLayout swipeRefresh;
+    private PopupWindow popupTodoAdd;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -58,13 +80,39 @@ public class TodoFragment extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
-                transaction.replace(R.id.fragment_todo,new TodoAddFragment());
-                transaction.addToBackStack(null);//添加到返回栈
-                transaction.commit();
+//                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+//                FragmentTransaction transaction = fragmentManager.beginTransaction();
+//                transaction.replace(R.id.fragment_todo,new TodoAddFragment());
+//                transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+////                transaction.addToBackStack(null);//添加到返回栈
+//                transaction.commit();
+
+                //通过popupwindow实现
+                load_popupwindow();
+
+                //淡化背景
+//                backgroundAlpha(0.5f);
+//                popupPhotoSelectorWindow.setOnDismissListener(() -> backgroundAlpha(1.0f));
+
+
+                //通过activity实现
+//                Intent intent = new Intent(getContext(),TodoAddActivity.class);
+//                startActivity(intent);
             }
         });
+
+        //滑动刷新
+        swipeRefresh = binding.todoSwipeRefresh;
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshTodo();
+            }
+        });
+
+        //传递数据
+//        EventBus.getDefault().register(this);
+
 
         return root;
     }
@@ -74,6 +122,7 @@ public class TodoFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+//        EventBus.getDefault().unregister(this);
     }
 
     public void initTodo(){
@@ -87,5 +136,75 @@ public class TodoFragment extends Fragment {
             todoList.add(todo);
         }
     }
+
+    @Subscribe
+    public void onEvent(String data) {
+        Todo todo = new Todo(data);
+        todoList.add(todo);
+        Log.d("todo",data);
+    }
+
+    public void refreshTodo(){
+        adapter.notifyDataSetChanged();
+        swipeRefresh.setRefreshing(false);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getContext(),"destroy",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Toast.makeText(getContext(),"pause",Toast.LENGTH_SHORT).show();
+    }
+
+    public void load_popupwindow(){
+        View popView = getLayoutInflater().inflate(R.layout.fragment_add_todo, null);
+        popupTodoAdd=new PopupWindow(popView, (int) (binding.getRoot().getWidth() * 0.8),
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        popupTodoAdd.setBackgroundDrawable(ResourcesCompat.getDrawable(getResources(),
+                R.drawable.round_outline, null));
+        popupTodoAdd.setOutsideTouchable(true);//点击其它退出
+        popupTodoAdd.setFocusable(true);//可编辑todo
+        popupTodoAdd.setAnimationStyle(R.style.popupWindow_anim_style);//设置动画
+
+        //设置点击事件
+        EditText editText = popView.findViewById(R.id.todo_edit);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.d("todo content",charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        Button button = popView.findViewById(R.id.add_todo_button);
+
+        button.setOnClickListener(view1 -> {
+            Toast.makeText(getContext(),"已保存",Toast.LENGTH_SHORT).show();
+//            EventBus.getDefault().post(editText.getText().toString());
+            todoList.add(0,new Todo(editText.getText().toString()));//将todo添加到第一个
+            refreshTodo();
+            popupTodoAdd.dismiss();
+        });
+
+
+        popupTodoAdd.showAtLocation(binding.getRoot(), Gravity.CENTER, 0, 0);
+    }
+
+
 
 }
